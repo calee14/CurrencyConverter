5389d9f6c4e7399feb5874709509475b6e9e59c0
calee14 <caplee1000@gmail.com>
2018-07-04 12:10:49 -0700

    initial commit

--- ConvertCurrency/ViewController.swift
+++ ConvertCurrency/ViewController.swift
@@ -10,16 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usContainer: UIView!
+    @IBOutlet weak var canadaContainer: UIView!
+    
+    @IBOutlet weak var firstCurrencyCard: UIStackView!
+    @IBOutlet weak var firstCurrencyImage: UIImageView!
+    @IBOutlet weak var firstCurrencyTitle: UILabel!
+    @IBOutlet weak var firstCurrencyAmount: BillAmountTextField!
+    
+    @IBOutlet weak var secondCurrencyCard: UIStackView!
+    @IBOutlet weak var secondCurrencyImage: UIImageView!
+    @IBOutlet weak var secondCurrencyTitle: UILabel!
+    @IBOutlet weak var secondCurrencyAmount: BillAmountTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        firstCurrencyAmount.calculateButtonAction = {
+            if self.firstCurrencyAmount.isFirstResponder {
+                self.firstCurrencyAmount.resignFirstResponder()
+            }
+            guard let billAmountText = self.firstCurrencyAmount.text,
+                let billAmount = Double(billAmountText) else {
+                    return
+            }
+            self.secondCurrencyAmount.text = String(billAmount * 1.31)
+        }
+        secondCurrencyAmount.calculateButtonAction = {
+            if self.secondCurrencyAmount.isFirstResponder {
+                self.secondCurrencyAmount.resignFirstResponder()
+            }
+            guard let billAmountText = self.secondCurrencyAmount.text,
+                let billAmount = Double(billAmountText) else {
+                    return
+            }
+            self.firstCurrencyAmount.text = String(billAmount / 1.31)
+        }
+        
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+        
+        let bottomBorderView = UIView(frame: CGRect(x: 0, y: self.usContainer.frame.size.height,
+                                                 width: self.usContainer.frame.size.width,
+                                                 height: 1))
+        bottomBorderView.backgroundColor = UIColor.gray
+        self.usContainer.addSubview(bottomBorderView)
+        let bottomBorderView_2 = UIView(frame: CGRect(x: 0, y: self.canadaContainer.frame.size.height,
+                                                    width: self.canadaContainer.frame.size.width,
+                                                    height: 1))
+        bottomBorderView_2.backgroundColor = UIColor.gray
+        self.canadaContainer.addSubview(bottomBorderView_2)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
-
-
 }
 
